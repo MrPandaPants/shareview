599c03e680f4a3a908596e5cbad39922d3750ffc
Randy Villanueva <randyv12@hotmail.com>
2013-07-27 01:42:40 -0700

    adding sqlite3 db, still a noob on debugging the db

--- src/com/example/shareviewapp/MainActivity.java
+++ src/com/example/shareviewapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.shareviewapp;
 
+import com.example.shareviewapp.UsersMapper;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +13,11 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        this.initializeDBOnce();
+        
         setContentView(R.layout.activity_main);
+        
     }
 
     @Override
@@ -17,4 +25,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+	private UsersMapper getUsersMapper() {
+		// TODO Auto-generated method stub
+		return new UsersMapper(this);
+	}
+	
+	public void initializeDBOnce() {
+		UsersMapper usersMapper=  this.getUsersMapper();
+		
+		usersMapper.createDBFirst();
+		
+	}
 }
---
+++ src/com/example/shareviewapp/UsersMapper.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package com.example.shareviewapp;
+
+import java.util.LinkedList;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author MrPandaPants
+ *
+ */
+public class UsersMapper extends SQLiteOpenHelper {
+
+        
+	public UsersMapper(Context context) {
+		 super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		 
+	}
+	
+	private static final int DATABASE_VERSION = 3;
+	
+    private static final String PASSWORDS_TABLE_NAME = "passwords";
+    
+    public static String getPasswordsTableName() {
+		return PASSWORDS_TABLE_NAME;
+	}
+
+	private static final String PASSWORDS_TABLE_CREATE = "create table if not exists" +
+    		" users ( id integer not null, " +
+    		"username varchar(255), password varchar(255), primary key(id));";
+    
+	private static final String DATABASE_NAME = "shareview.sqlite"; 
+	
+	
+	public void deleteById(Integer id) {
+		SQLiteDatabase sq = getWritableDatabase();
+		SQLiteStatement cs = sq.compileStatement("delete from users where id = ?;");
+		
+		cs.bindString(1, id.toString());
+		cs.execute();
+		cs.close();
+	}
+	
+	public void createDBFirst() {
+		SQLiteDatabase sq = getWritableDatabase();
+		SQLiteStatement cs = sq.compileStatement(PASSWORDS_TABLE_CREATE);
+		
+		cs.execute();
+		cs.close();
+	}
+	
+	public int createPassword(String name, String value) {
+		SQLiteDatabase sq = getWritableDatabase();
+		SQLiteStatement cs = sq.compileStatement("insert into users values (null, ?, ?);");
+		
+		cs.bindString(1, name);
+		cs.bindString(2, value);
+		cs.execute();
+		cs.close();
+		
+		
+		return 1;
+	}
+
+	public User[] fetchAll() {
+		SQLiteDatabase sq = getReadableDatabase(); 
+		Cursor c = sq.rawQuery("SELECT * FROM users order by id desc;", null);
+	 
+		LinkedList<User> lstr = new LinkedList<User>();
+		
+		c.moveToFirst();
+		while (!c.isLast()) {
+			 
+			lstr.add(new User(Integer.parseInt(c.getString(0)),
+					c.getString(1), c.getString(2)));
+			 
+			c.moveToNext();
+		}
+		c.close();
+		Object[] array = lstr.toArray();
+		User[] tt = new User[array.length];
+		for (int j = 0; j < array.length; j++) {
+			tt[j] = (User) array[j];
+			
+		}
+		return tt;
+		
+		
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+	 */
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+	
+		db.execSQL(PASSWORDS_TABLE_CREATE);
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
